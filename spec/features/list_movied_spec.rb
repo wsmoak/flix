00f735fec17f5c78b106311719d487da89cbd368
Wendy Smoak <wsmoak@gmail.com>
2015-05-09 06:33:09 -0400

    Add test, controller and view for displaying hard-coded text

---
+++ spec/features/list_movied_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "Viewing the list of movies" do
+
+  it "shows the movies" do
+    visit "http://example.com/movies"
+
+    expect(page).to have_text("3 Movies")
+  end
+
+end
