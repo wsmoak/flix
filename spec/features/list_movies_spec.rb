40370f8a2c53f962ddb8d892bd07f1f414eadbc0
Wendy Smoak <wsmoak@gmail.com>
2015-05-10 20:11:11 -0400

    Include all attributes on the page and update spec

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -26,6 +26,12 @@ describe "Viewing the list of movies" do
     expect(page).to have_text(movie1.title)
     expect(page).to have_text(movie2.title)
     expect(page).to have_text(movie3.title)
+
+    expect(page).to have_text(movie1.rating)
+    expect(page).to have_text(movie1.description[0..10])
+    expect(page).to have_text(movie1.released_on)
+    expect(page).to have_text("$318,412,101.00")
+
   end
 
 end
