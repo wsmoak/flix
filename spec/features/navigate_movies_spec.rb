0398553453d6112c758118c8e7c8d4812d191b6e
Wendy Smoak <wsmoak@gmail.com>
2015-05-12 21:27:24 -0400

    Use a support method to avoid duplication

--- spec/features/navigate_movies_spec.rb
+++ spec/features/navigate_movies_spec.rb
@@ -2,11 +2,7 @@ require 'spec_helper'
 
 describe "Navigating movies" do
   it "allows navigation from the detail page to the listing page" do
-    movie = Movie.create(title: "Iron Man",
-                         rating: "PG-13",
-                         total_gross: 318412101.00,
-                         description: "Tony Stark builds an armored suit to fight the throes of evil",
-                         released_on: "2008-05-02")
+    movie = Movie.create(movie_attributes)
 
     visit movie_url(movie)
 
@@ -16,11 +12,7 @@ describe "Navigating movies" do
   end
 
     it "allows navigation from the listing page to the detail page" do
-    movie = Movie.create(title: "Iron Man",
-                         rating: "PG-13",
-                         total_gross: 318412101.00,
-                         description: "Tony Stark builds an armored suit to fight the throes of evil",
-                         released_on: "2008-05-02")
+    movie = Movie.create(movie_attributes)
 
     visit movies_url
 
--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -3,17 +3,13 @@ require 'spec_helper'
 describe "Viewing an individual movie" do
 
   it "shows the movie's details" do
-    movie = Movie.create(title: "Iron Man",
-                       rating: "PG-13",
-                       total_gross: 318412101.00,
-                       description: "Tony Stark builds an armored suit to fight the throes of evil",
-                       released_on: "2008-05-02")
+    movie = Movie.create(movie_attributes(total_gross: 123456789))
 
     visit movie_url(movie)
 
     expect(page).to have_text(movie.title)
     expect(page).to have_text(movie.rating)
-    expect(page).to have_text("$318,412,101.00")
+    expect(page).to have_text("$123,456,789.00")
     expect(page).to have_text(movie.description)
     expect(page).to have_text(movie.released_on)
   end
