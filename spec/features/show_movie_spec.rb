abbb5307d87118e35d57c8ba51bca68abd38171d
Wendy Smoak <wsmoak@gmail.com>
2015-05-14 09:08:48 -0400

    Add tests and implement displaying 'Flop' if the movie grossed less than 0M

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -9,9 +9,24 @@ describe "Viewing an individual movie" do
 
     expect(page).to have_text(movie.title)
     expect(page).to have_text(movie.rating)
-    expect(page).to have_text("$123,456,789.00")
     expect(page).to have_text(movie.description)
     expect(page).to have_text(movie.released_on)
+  end
+
+  it "shows the total gross if the total gross exceeds $50M" do
+    movie = Movie.create(movie_attributes(total_gross: 60000000.00))
+
+    visit movie_url(movie)
+
+    expect(page).to have_text("$60,000,000.00")
+  end
+
+  it "shows 'Flop!' if the total gross is less than $50M" do
+    movie = Movie.create(movie_attributes(total_gross: 3000000.00))
+
+    visit movie_url(movie)
+
+    expect(page).to have_text("Flop!")
   end
 
 end
