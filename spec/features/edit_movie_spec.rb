66178b429bfe33ae5c90dcb7fcdb1d6f662cbed4
Wendy Smoak <wsmoak@gmail.com>
2015-05-17 22:03:02 -0400

    Process the form submit and update the database

--- spec/features/edit_movie_spec.rb
+++ spec/features/edit_movie_spec.rb
@@ -12,6 +12,14 @@ describe "Editing a movie" do
     expect(current_path).to eq(edit_movie_path(movie))
 
     expect(find_field('Title').value).to eq(movie.title)
+
+    fill_in 'Title', with: "Updated Movie Title"
+
+    click_button 'Update Movie'
+
+    expect(current_path).to eq(movie_path(movie))
+
+    expect(page).to have_text('Updated Movie Title')
   end
 
 end
