b8584c7526112646764f956167b07efd36c9cf8f
Wendy Smoak <wsmoak@gmail.com>
2015-06-13 21:46:37 -0400

    Update tests for new design

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -22,14 +22,13 @@ describe "Viewing the list of movies" do
                       released_on: "2002-05-03")
     visit movies_url
 
-    expect(page).to have_text("3 Movies")
     expect(page).to have_text(movie1.title)
     expect(page).to have_text(movie2.title)
     expect(page).to have_text(movie3.title)
 
     expect(page).to have_text(movie1.rating)
     expect(page).to have_text(movie1.description[0..10])
-    expect(page).to have_text(movie1.released_on)
+    expect(page).to have_text(movie1.released_on.year)
     expect(page).to have_text("$318,412,101.00")
 
   end
