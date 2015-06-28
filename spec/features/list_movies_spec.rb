0bc78d3f60af6e38db5195827cb133708057784e
Wendy Smoak <wsmoak@gmail.com>
2015-06-28 16:31:39 -0400

    Introduce validations

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -6,18 +6,21 @@ describe "Viewing the list of movies" do
     movie1 = Movie.create(title: "Iron Man",
                       rating: "PG-13",
                       total_gross: 318412101.00,
+                      duration: 90,
                       description: "Tony Stark builds an armored suit to fight the throes of evil",
                       released_on: "2008-05-02")
 
     movie2 = Movie.create(title: "Superman",
                       rating: "PG",
                       total_gross: 134218018.00,
+                      duration: 120,
                       description: "Clark Kent grows up to be the greatest super-hero",
                       released_on: "1978-12-15")
 
     movie3 = Movie.create(title: "Spider-Man",
                       rating: "PG-13",
                       total_gross: 403706375.00,
+                      duration: 75,
                       description: "Peter Parker gets bit by a genetically modified spider",
                       released_on: "2002-05-03")
     visit movies_url
